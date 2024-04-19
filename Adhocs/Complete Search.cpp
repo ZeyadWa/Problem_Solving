ce133bf3faebc8168fdcebb2460bd212a3c98590
Zeyad <zeyadw526@gmail.com>
2024-04-19 02:22:58 +0200

    .

--- Adhocs/Complete Search.cpp
+++ Adhocs/Complete Search.cpp
@@ -1,11 +1,10 @@
 for( int mask = 0 ; mask < ( 1 << 9 ) ; mask++ )
-{
-        
+{ 
     for( int j = 0 ; j < 9 ; j++ )
     {
         if( ( mask >> j ) & 1 )
         {
-                //operations
+                // operations
         }
     }
 }
